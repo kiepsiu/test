ef10a1305f65e5ea6307c4982ff65267416ecf81
Krzysztof Kieps <kiepsiu3@gmail.com>
2022-08-03 18:34:52 +0200

    initial commit

---
+++ pages/michu.js
@@ -0,0 +1,5 @@
+const Page = () =>{
+    console.log('Karzel');
+    return <h1 style={{ background: "gray"}}>Hello world!</h1>
+};
+export default Page
---
+++ pages/react.js
@@ -0,0 +1,1 @@
+ReactDOM.render(<p>Hello,Chuju!</p>)
