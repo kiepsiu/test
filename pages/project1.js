9d9ba122873e4a9836cb72ff85755f515ee3e058
Koniuszy <44303865+koniuszy@users.noreply.github.com>
2022-08-04 16:14:00 +0200

    split components without formatting

--- pages/project1.js
+++ pages/project1.js
@@ -1,15 +1,22 @@
-const Fun = () => {
-  return (
-    <div style={{ textAlign: 'center', color: 'blue' }}>
-      <h1>Fun facts about React</h1>
-      <h3>Michal op programista</h3>
-      <ul>
+const List = () => {
+
+    return (
+        <ul>
         <li>Was first released in 2013</li>
         <li>Was originally created by Jordan Walke</li>
         <li>Has well over 100k stars on Github</li>
         <li>Is maintaind by Facebook</li>
         <li>Powers thousands of enterprise apps, including mobile apps</li>
       </ul>
+    )
+}
+
+const Fun = () => {
+  return (
+    <div style={{ textAlign: 'center', color: 'blue' }}>
+      <h1>Fun facts about React</h1>
+      <h3>Michal op programista</h3>
+     <List />
     </div>
   );
 };
