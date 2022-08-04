98ca0536c751cfa140470ec273e2f6df16302d44
Krzysztof Kieps <kiepsiu3@gmail.com>
2022-08-04 16:36:15 +0200

    i'm trying put one picter intu website and look what happens!!!

--- pages/project1.js
+++ pages/project1.js
@@ -1,23 +1,26 @@
 const List = () => {
-
-    return (
-        <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100k stars on Github</li>
-        <li>Is maintaind by Facebook</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
-    )
-}
+  return (
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100k stars on Github</li>
+      <li>Is maintaind by Facebook</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+  );
+};
 
 const Fun = () => {
   return (
-    <div style={{ textAlign: 'center', color: 'blue' }}>
+    <div style={{ textAlign: 'center', color: 'black' }}>
       <h1>Fun facts about React</h1>
       <h3>Michal op programista</h3>
-     <List />
+      <List />
+      <Img />
     </div>
   );
+};
+const Img = () => {
+  return <img src="/home/krzysztof/Desktop/nextjs-blog/img/reactLogo.png" />;
 };
 export default Fun;
