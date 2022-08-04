0a9ad061cb3e4858dbbfad6ba333e5e378c75290
Krzysztof Kieps <kiepsiu3@gmail.com>
2022-08-04 16:12:51 +0200

    help 2

--- pages/project1.js
+++ pages/project1.js
@@ -1,4 +1,16 @@
-const fun = () =>{
-    return <div><h1>Fun facts about React</h1><ul><li>Was first released in 2013</li><li>Was originally created by Jordan Walke</li><li>Has well over 100k stars on Github</li><li>Is maintaind by Facebook</li><li>Powers thousands of enterprise apps, including mobile apps</li></ul></div>
-}
-export default fun
+const Fun = () => {
+  return (
+    <div style={{ textAlign: 'center', color: 'blue' }}>
+      <h1>Fun facts about React</h1>
+      <h3>Michal op programista</h3>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100k stars on Github</li>
+        <li>Is maintaind by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </div>
+  );
+};
+export default Fun;
--- pages/react.js
+++ pages/react.js
@@ -1,5 +1,13 @@
 const Page = () => {
-    return <div><h1>Hello,Chuju!</h1><ul><li>Thing1</li><li>Thing2</li></ul></div>
+  return (
+    <div>
+      <h1>Hello,Chuju!</h1>
+      <ul>
+        <li>Thing1</li>
+        <li>Thing2</li>
+      </ul>
+    </div>
+  );
 };
 
-export default Page
+export default Page;
