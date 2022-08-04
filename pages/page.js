4198dcb25829cd7f0705330b1ef6f6bd991c9d4a
Krzysztof Kieps <kiepsiu3@gmail.com>
2022-08-04 15:14:22 +0200

    Wez to sprawdz czy nie idzie opisac tego inaczej chodzi mi o to czy ja musze wszystko w jednym const pisac? czy dam rade zrobic kilka const?

---
+++ pages/page.js
@@ -0,0 +1,4 @@
+const page =()=>{
+   return <div><h1>My awesome website in reacte</h1><h2>Reasone why i love reacte</h2><ol><li>Why?</li><li>Why not?</li><li>Michal to Op programista xD</li></ol></div> 
+}
+export default page
